3e4c77265a7079db6a437c73bee56c0eaad0f643
DESKTOP-M262J75\1강의실 <dlwldus12341234@gmail.com>
2020-11-16 16:55:53 +0900

    initial

---
+++ js/common.js
@@ -0,0 +1,9 @@
+// $(document).ready(function () {
+//     new fullpage('#full-page', {
+//         licenseKey: '',
+//         sectionColor: ['#ff0000', '#00ff00', '#0000ff', '#aaaaaa'],
+//         navigation: true,
+//         navigationTooltips: ['Home', 'About', 'Contact', 'Xtras'],
+//         scrollingSpeed: 2000,
+//     })
+// })
